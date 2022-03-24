9489aab589c0587a82b7af9e046e8d35ffd45d1d
charlottengu <98812724+charlottengu@users.noreply.github.com>
2022-03-24 10:22:07 +0100

    v2

---
+++ gender_inequity/gendergap.js
@@ -0,0 +1,19 @@
+fetch('https://stats.oecd.org/SDMX-JSON/data/DEC_I/AUS+AUT+BEL+CAN+CHL+COL+CRI+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+OECD+BGR+HRV+CYP+MLT+ROU.MEN+WOMEN+MW.LPI+P5010+P9010+P9050+HPI+GWG1+GWG5+GWG9/all?startTime=2005&endTime=2020&dimensionAtObservation=allDimensions')
+.then( (data) => {
+    if(data.ok){
+        return data.json()
+        
+    } 
+})
+.then( gendergap => generateHtml(gendergap))
+.catch( error => console.error('Error:', error))
+
+const generateHtml = (data) => {
+    console.log(data)
+    const html = `
+        <div class="name">${data}</div>
+    `
+    const gendergapDiv = document.querySelector('.gendergap')
+    gendergapDiv.innerHTML = html
+
+}
