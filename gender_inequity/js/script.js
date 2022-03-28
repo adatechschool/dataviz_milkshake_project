845c8701d389fe5bca34eba61aa775ac6a316c4a
charlottengu <98812724+charlottengu@users.noreply.github.com>
2022-03-28 17:08:31 +0200

    feat javascript front

--- gender_inequity/js/script.js
+++ gender_inequity/js/script.js
@@ -1,8 +1,21 @@
-document.getElementById("country").onclick = function() {getCountry()};
+document.getElementById("allemagne").onclick = function() {getAllemagne()};
 
-function getCountry () {
-    document.getElementById("textecountry").innerHTML =  `<h5>Allemagne</h5>;`
+function getAllemagne () {
+    document.getElementById("textecountry").innerHTML =  `<h5>En Allemagne, les femmes gagnent 11.82 de moins que les hommes</h5>`;
+    document.getElementById("flagcountry").src="css/flags/allemagneflag.png";
+}
+
+document.getElementById("france").onclick = function() {getFrance()};
+
+function getFrance () {
+    document.getElementById("textecountry").innerHTML =  `<h5>En France, les femmes gagnent 11.82 de moins que les hommes</h5>`;
+    document.getElementById("flagcountry").src="css/flags/fr.svg";
 }
 
 
+document.getElementById("canada").onclick = function() {getCanada()};
 
+function getCanada () {
+    document.getElementById("textecountry").innerHTML =  `<h5>Au Canada, les femmes gagnent 11.82 de moins que les hommes</h5>`;
+    document.getElementById("flagcountry").src="css/flags/canadaflag.png";
+}
